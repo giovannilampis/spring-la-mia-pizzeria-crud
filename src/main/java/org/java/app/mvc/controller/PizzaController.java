ad79bdc9a07be67eff4c4f0fd31e98c30ccd729f
giovannilampis <giolampis@hotmail.com>
2023-10-10 15:49:02 +0200

    add pizza-create.html to project

--- src/main/java/org/java/app/mvc/controller/PizzaController.java
+++ src/main/java/org/java/app/mvc/controller/PizzaController.java
@@ -8,10 +8,15 @@ import org.java.app.pizzeria.serv.PizzaServ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -44,5 +49,36 @@ public class PizzaController {
 		model.addAttribute("pizza", pizza);
 		
 		return "pizza-show";
+	}
+	
+	@GetMapping("/create")
+	public String getCreateFormt(Model model) {
+		
+		model.addAttribute("pizza", new Pizza());
+		
+		return "pizza-create";
+	}
+	@PostMapping("/pizzas/create")
+	public String storeNewPizza(
+			@Valid @ModelAttribute Pizza pizza,
+			BindingResult bindingResult,
+			Model model
+		) {
+		
+		System.out.println("pizza:\n" + pizza);
+		
+		if (bindingResult.hasErrors()) {
+			
+			System.out.println("Errors");
+			bindingResult.getAllErrors().stream()
+					.map(e -> e.getDefaultMessage())
+				.forEach(System.out::println);
+			
+			return "pizza-create";
+		}
+		
+		pizzaServ.save(pizza);
+		
+		return "redirect:/";
 	}
 }
